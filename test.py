ee91f6d505e861bbcaeadfacbda6357fd235eb0e
WOONG-BIN <idb0227@naver.com>
2021-10-05 22:44:42 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("호동이의 중간고사 평균")
+호동이의 중간고사 평균
+국어 = 100
+영어 = 90
+수학 = 99
+사회 = 96
+과학 = 88
+
+국어+영어+수학+사회+과학
+473
+
+473/5
+94.6
+
+평균
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    평균
+NameError: name '평균' is not defined
+평균 = 94.6
