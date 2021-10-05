cd019de300923943280fdc505470910f7ddb6a6f
WOONG-BIN <idb0227@naver.com>
2021-10-05 23:03:06 +0900

    Update test.py
    중간고사 > 기말고사로 변경

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
-print("호동이의 중간고사 평균")
+print("호동이의 기말고사 평균")
 호동이의 기말고사  평균
 국어 = 100
 영어 = 90
